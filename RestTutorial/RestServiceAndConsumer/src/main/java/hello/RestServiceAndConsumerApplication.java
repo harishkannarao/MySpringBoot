eb7479db3262c049a5e47c2162b90750bf9bb017
Harish Kannarao <harish.gtec@gmail.com>
2015-10-06 12:22:00 +0100

    Tech: Introduced configurableApplicationContext

--- RestTutorial/RestServiceAndConsumer/src/main/java/hello/RestServiceAndConsumerApplication.java
+++ RestTutorial/RestServiceAndConsumer/src/main/java/hello/RestServiceAndConsumerApplication.java
@@ -3,13 +3,20 @@ package hello;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RestServiceAndConsumerApplication {
+    private static ConfigurableApplicationContext configurableApplicationContext;
+
     public static void main(String[] args) {
-        SpringApplication.run(RestServiceAndConsumerApplication.class, args);
+        configurableApplicationContext = SpringApplication.run(RestServiceAndConsumerApplication.class, args);
+    }
+
+    public static ConfigurableApplicationContext getConfigurableApplicationContext() {
+        return configurableApplicationContext;
     }
 
     @Bean
--- RestTutorial/ThirdPartyRestServiceStub/src/main/java/hello/ThirdPartyRestServiceStubApplication.java
+++ RestTutorial/ThirdPartyRestServiceStub/src/main/java/hello/ThirdPartyRestServiceStubApplication.java
@@ -2,10 +2,17 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ThirdPartyRestServiceStubApplication {
+    private static ConfigurableApplicationContext configurableApplicationContext;
+
     public static void main(String[] args) {
-        SpringApplication.run(ThirdPartyRestServiceStubApplication.class, args);
+        configurableApplicationContext = SpringApplication.run(ThirdPartyRestServiceStubApplication.class, args);
+    }
+
+    public static ConfigurableApplicationContext getConfigurableApplicationContext() {
+        return configurableApplicationContext;
     }
 }
