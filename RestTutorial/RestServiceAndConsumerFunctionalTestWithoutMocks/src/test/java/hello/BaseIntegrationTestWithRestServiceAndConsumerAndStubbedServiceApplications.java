1304055cd66804762bea81fab3d472fe59c4a294
Harish Kannarao <harish.gtec@gmail.com>
2015-10-20 17:25:59 +0100

    - Created an example of rest service and consumer integration test without mocks

---
+++ RestTutorial/RestServiceAndConsumerFunctionalTestWithoutMocks/src/test/java/hello/BaseIntegrationTestWithRestServiceAndConsumerAndStubbedServiceApplications.java
@@ -0,0 +1,49 @@
+package hello;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {RestServiceAndConsumerApplication.class, RestServiceAndConsumerApplication.class})
+@WebIntegrationTest({
+        "server.port=0",
+        "management.port=0"
+})
+public abstract class BaseIntegrationTestWithRestServiceAndConsumerAndStubbedServiceApplications {
+    public static final String thirdPartyQuoteEndpointStringFormat = "http://localhost:%s/thirdparty/quote";
+
+    @org.springframework.beans.factory.annotation.Value("${local.server.port}")
+    protected int port;
+    @Autowired
+    @Qualifier("myThirdPartyRestQuoteClientImpl")
+    protected ThirdPartyRestQuoteClientImpl thirdPartyRestQuoteClientImpl;
+    protected RestTemplate restTemplate = getRestTemplate();
+
+    @Before
+    public void setup() {
+        thirdPartyRestQuoteClientImpl.setThirdPartyRestQuoteServiceUrl(getThirdPartyQuoteEndpointString());
+    }
+
+    private RestTemplate getRestTemplate() {
+        List<ClientHttpRequestInterceptor> interceptors = new ArrayList<ClientHttpRequestInterceptor>();
+        interceptors.add(new JsonHeaderInterceptor());
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setInterceptors(interceptors);
+        return restTemplate;
+    }
+
+    private String getThirdPartyQuoteEndpointString() {
+        return String.format(thirdPartyQuoteEndpointStringFormat, port);
+    }
+}
+
---
+++ RestTutorial/RestServiceAndConsumerFunctionalTestWithoutMocks/src/test/java/hello/QuoteControllerIT.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class QuoteControllerIT extends BaseIntegrationTestWithRestServiceAndConsumerAndStubbedServiceApplications{
+    public static final String quoteEndpointStringFormat = "http://localhost:%s/quote";
+
+    private String getQuoteEndpointString() {
+        return String.format(quoteEndpointStringFormat, port);
+    }
+
+    @Test
+    public void getQuote_shouldReturnQuoteDetails_fromThirdPartyRestService() {
+        Quote result = restTemplate.getForObject(getQuoteEndpointString(), Quote.class);
+        assertEquals("success", result.getType());
+        assertEquals("Working with Spring Boot is like pair-programming with the Spring developers.", result.getValue().getQuote());
+        assertEquals(new Long(1L), result.getValue().getId());
+    }
+}
