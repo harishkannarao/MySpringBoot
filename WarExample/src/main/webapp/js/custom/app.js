7d91c79f636223bc12c083ea22c8178d9909e331
Harish Kannarao <harish.gtec@gmail.com>
2016-04-03 22:18:06 +0100

    Tech: Getting configData using angularjs provider
    * Creating a provider for configData service
    * Manually bootstrapping angularjs after fetching configData

--- WarExample/src/main/webapp/js/custom/app.js
+++ WarExample/src/main/webapp/js/custom/app.js
@@ -17,4 +17,47 @@ springBootAngularWarApp.config(['$routeProvider', function($routeProvider) {
       });
 }]);
 
+springBootAngularWarApp.provider('configData', function () {
+   var options = {};
+   this.setConfig = function (opt) {
+       angular.extend(options, opt);
+   };
+   this.$get = [function () {
+       if (!options) {
+           throw new Error('Config options must be configured');
+       }
+       return angular.copy(options);
+   }];
+});
+
+angular.element(document).ready(function () {
+
+    fetchData().then(bootstrapApplication);
+
+    function fetchData() {
+        var initInjector = angular.injector(["ng"]);
+        var $http = initInjector.get("$http");
+
+        var propertiesRequest = {
+            method: 'GET',
+            url: '/properties',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            data: {}
+        }
+        return $http(propertiesRequest).success(function(data) {
+              angular.module('springBootAngularWarApp').config(['configDataProvider', function (configDataProvider) {
+                configDataProvider.setConfig(data);
+              }]);
+        });
+    }
+
+    function bootstrapApplication() {
+        angular.bootstrap(document, ['springBootAngularWarApp']);
+    }
+
+});
+
 
--- WarExample/src/main/webapp/js/custom/controllers.js
+++ WarExample/src/main/webapp/js/custom/controllers.js
@@ -1,16 +1,5 @@
 var springBootAngularWarControllers = angular.module('springBootAngularWarControllers', []);
 
-springBootAngularWarControllers.controller('HomeCtrl', ['$scope', '$http', function ($scope, $http) {
-    var propertiesRequest = {
-        method: 'GET',
-        url: '/properties',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        data: {}
-    }
-    $http(propertiesRequest).success(function(data) {
-          $scope.properties = data;
-    });
+springBootAngularWarControllers.controller('HomeCtrl', ['$scope', 'configData', function ($scope, configData) {
+    $scope.properties = configData;
 }]);
