e844e7297a2c8407724a4f7aeeaea56db1227911
Harish Kannarao <harish.gtec@gmail.com>
2016-05-14 17:25:09 +0100

    Tech: Creating test for dev profile

---
+++ WarExample/src/test/java/hello/HomePageDevProfileIT.java
@@ -0,0 +1,23 @@
+package hello;
+
+import hello.pageobjects.HomePageDevProfile;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.assertEquals;
+
+public class HomePageDevProfileIT extends AbstractDevProfileBaseIT {
+
+    @Autowired
+    private HomePageDevProfile homePageDevProfile;
+
+    @Test
+    public void shouldDisplayOverriddenMessagesOnHomePage() throws Exception {
+        homePageDevProfile.navigate();
+        assertEquals("Message1 from spring-boot-default-config/application.properties", homePageDevProfile.getMessage1());
+        assertEquals("Message2 from spring-boot-default-config/application.properties", homePageDevProfile.getMessage2());
+        assertEquals("Message3 from spring-boot-default-config/application.properties", homePageDevProfile.getMessage3());
+        assertEquals("Message4 from spring-boot-default-config/application.properties", homePageDevProfile.getMessage4());
+        assertEquals("Message5 from spring-boot-default-config/application-dev.properties", homePageDevProfile.getMessage5());
+    }
+}
---
+++ WarExample/src/test/java/hello/pageobjects/HomePageDevProfile.java
@@ -0,0 +1,49 @@
+package hello.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HomePageDevProfile {
+    private final WebDriver webDriver;
+    private final String homePageDevProfileUrl;
+
+    @Autowired
+    public HomePageDevProfile(WebDriver webDriver,
+                              @Value("${warExampleDevProfileIndexPageUrl}") String warExampleIndexPageUrl) {
+        this.webDriver = webDriver;
+        this.homePageDevProfileUrl = warExampleIndexPageUrl + "#/home";
+    }
+
+    public boolean isOnPage() {
+        return webDriver.findElements(By.id("qa-home-page-id")).size() > 0;
+    }
+
+    public void navigate() {
+        webDriver.navigate().to(homePageDevProfileUrl);
+        webDriver.navigate().refresh();
+        WebDriverWait waitForHomePageElement = new WebDriverWait(webDriver, 10);
+        waitForHomePageElement.until(ExpectedConditions.presenceOfElementLocated(By.id("qa-home-page-id")));
+    }
+
+    public String getMessage1() {
+        return webDriver.findElement(By.id("message1")).getText();
+    }
+    public String getMessage2() {
+        return webDriver.findElement(By.id("message2")).getText();
+    }
+    public String getMessage3() {
+        return webDriver.findElement(By.id("message3")).getText();
+    }
+    public String getMessage4() {
+        return webDriver.findElement(By.id("message4")).getText();
+    }
+    public String getMessage5() {
+        return webDriver.findElement(By.id("message5")).getText();
+    }
+}
