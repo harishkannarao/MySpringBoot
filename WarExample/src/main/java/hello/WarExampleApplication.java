f6270f3b68632878b2d45834dea4d053338bb1d5
Harish Kannarao <harish.gtec@gmail.com>
2016-05-14 18:01:03 +0100

    Tech: Reading the active profile using environment variable

--- WarExample/src/main/java/hello/WarExampleApplication.java
+++ WarExample/src/main/java/hello/WarExampleApplication.java
@@ -6,10 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 @SpringBootApplication
 public class WarExampleApplication extends SpringBootServletInitializer {
@@ -34,9 +31,14 @@ public class WarExampleApplication extends SpringBootServletInitializer {
 			configLocations.add(configLocationBuilder.toString());
 		}
 		String springConfigValues = StringUtils.collectionToCommaDelimitedString(configLocations);
+		String warExampleActiveProfiles = System.getenv("WAR_EXAMPLE_ACTIVE_PROFILES");
 
 		Properties props = new Properties();
+		if(warExampleActiveProfiles != null) {
+			props.put("spring.profiles.active", warExampleActiveProfiles);
+		}
 		props.put("spring.config.location", springConfigValues);
+
 		return props;
 	}
 
@@ -45,6 +47,13 @@ public class WarExampleApplication extends SpringBootServletInitializer {
 		boolean containsSpringConfigParam = arguments.stream().anyMatch(s -> s.contains("spring.config.location"));
 		if(!containsSpringConfigParam) {
 			arguments.add(getSpringConfigParam());
+		}
+		boolean containsActiveProfilesParam = arguments.stream().anyMatch(s -> s.contains("spring.profiles.active"));
+		if(!containsActiveProfilesParam) {
+			Optional<String> springActiveProfilesParam = getSpringActiveProfilesParam();
+			if(springActiveProfilesParam.isPresent()) {
+				arguments.add(springActiveProfilesParam.get());
+			}
 		}
 
 		String[] modifiedArgs = new String[arguments.size()];
@@ -69,6 +78,15 @@ public class WarExampleApplication extends SpringBootServletInitializer {
 		}
 		String springConfigValues = StringUtils.collectionToCommaDelimitedString(configLocations);
 		return String.format("--spring.config.location=%s", springConfigValues);
+	}
+
+	private static Optional<String> getSpringActiveProfilesParam() {
+		Optional<String> result = Optional.empty();
+		String warExampleActiveProfiles = System.getenv("WAR_EXAMPLE_ACTIVE_PROFILES");
+		if(warExampleActiveProfiles != null) {
+			result = Optional.of(String.format("--spring.profiles.active=%s", warExampleActiveProfiles));
+		}
+		return result;
 	}
 
 }
