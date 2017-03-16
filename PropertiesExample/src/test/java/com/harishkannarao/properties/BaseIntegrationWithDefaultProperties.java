d3d47522e925cd799311b3345cf9855287fa9efa
harishkannarao <harish.gtec@gmail.com>
2017-03-16 22:19:13 +0530

    Using random port for properties example testing and migrating to yaml files

--- PropertiesExample/src/test/java/com/harishkannarao/properties/BaseIntegrationWithDefaultProperties.java
+++ PropertiesExample/src/test/java/com/harishkannarao/properties/BaseIntegrationWithDefaultProperties.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,12 +16,9 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
                 PropertiesApplication.class,
                 TestConfigurationPropertiesApplication.class
         },
-        webEnvironment = DEFINED_PORT,
-        properties = {
-                "server.port=${RANDOM_PORT_1:8280}",
-                "management.port=${RANDOM_PORT_2:8281}",
-                "properties.endpoint.url=http://localhost:${server.port}"
-        })
+        webEnvironment = DEFINED_PORT
+)
+@ActiveProfiles(value = {"default", "properties-test"})
 public abstract class BaseIntegrationWithDefaultProperties {
     @Autowired
     @Qualifier("myTestRestTemplate")
