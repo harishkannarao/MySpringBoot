1760533f01dc1a0a3666bf5ea9fad60cd3ceaaa2
harishkannarao <harish.gtec@gmail.com>
2017-03-17 18:16:30 +0530

    Testing 404 status

--- RestExample/RestServiceAndConsumerFunctionalTest/src/test/java/com/harishkannarao/rest/functional/ErrorPageIT.java
+++ RestExample/RestServiceAndConsumerFunctionalTest/src/test/java/com/harishkannarao/rest/functional/ErrorPageIT.java
@@ -2,18 +2,26 @@ package com.harishkannarao.rest.functional;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.Assert.assertEquals;
 
 public class ErrorPageIT extends BaseIntegration {
-    @org.springframework.beans.factory.annotation.Value("${invalidPageUrl}")
-    public String invalidPageUrl;
+    @org.springframework.beans.factory.annotation.Value("${nonExistentPageUrl}")
+    public String nonExistentPageUrl;
 
     @Test
-    public void shouldGetErrorPage() {
-        webDriver.navigate().to(invalidPageUrl);
+    public void shouldGetNotFoundErrorPageForNonExistentPage() {
+        webDriver.navigate().to(nonExistentPageUrl);
         String errorStatus = webDriver.findElement(By.id("errorStatus")).getText();
-        assertEquals(invalidPageUrl, webDriver.getCurrentUrl());
+        assertEquals(nonExistentPageUrl, webDriver.getCurrentUrl());
         assertEquals("404 Not Found", errorStatus);
+    }
+
+    @Test
+    public void shouldGetNotFoundStatusForNonExistentPage() {
+        ResponseEntity<String> response = testRestTemplate.getForEntity(nonExistentPageUrl, String.class);
+        assertEquals(response.getStatusCode(), HttpStatus.NOT_FOUND);
     }
 }
