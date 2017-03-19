9f71d9f06c10bdefa1b241c4de228463474ac99b
harishkannarao <harish.gtec@gmail.com>
2017-03-19 16:06:06 +0530

    Preparing war example for multiple profile

--- WarExample/src/test/java/com/harishkannarao/war/AbstractBaseIT.java
+++ WarExample/src/test/java/com/harishkannarao/war/AbstractBaseIT.java
@@ -2,6 +2,7 @@ package com.harishkannarao.war;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
@@ -14,7 +15,8 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
         },
         webEnvironment = DEFINED_PORT,
         properties = {
-                "server.port=${RANDOM_PORT_5:8185}",
                 "spring.config.location=classpath:/spring-boot-default-config/"
-        })
+        }
+)
+@ActiveProfiles(value = {"integration-test"})
 public abstract class AbstractBaseIT {}
--- WarExample/src/test/java/com/harishkannarao/war/AbstractDevProfileBaseIT.java
+++ WarExample/src/test/java/com/harishkannarao/war/AbstractDevProfileBaseIT.java
@@ -2,6 +2,7 @@ package com.harishkannarao.war;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
@@ -11,8 +12,8 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
         classes = {TestConfigurationWarExampleApplication.class},
         webEnvironment = DEFINED_PORT,
         properties = {
-                "server.port=${RANDOM_PORT_5:8186}",
-                "spring.config.location=classpath:/spring-boot-default-config/",
-                "spring.profiles.active=dev"
-        })
+                "spring.config.location=classpath:/spring-boot-default-config/"
+        }
+)
+@ActiveProfiles(value = {"dev", "integration-test", "dev-integration-test"})
 public abstract class AbstractDevProfileBaseIT {}
