8bdd2ec61065a9542a174c02243f022104d360a5
harishkannarao <harish.gtec@gmail.com>
2020-04-19 22:12:20 +0530

    Revert "Adding additional request path for health check controller"
    This reverts commit 2c8f5adbda99c678a0090e88ee7f561efcd788ab.

--- JdbcExample/JdbcApplication/src/main/java/com/harishkannarao/jdbc/controller/HealthCheckRestController.java
+++ JdbcExample/JdbcApplication/src/main/java/com/harishkannarao/jdbc/controller/HealthCheckRestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = {"/health-check","/health","/health-check-alb","/"}, produces = {MediaType.APPLICATION_JSON_VALUE})
+@RequestMapping(value = "/health-check", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class HealthCheckRestController {
 
     @RequestMapping
