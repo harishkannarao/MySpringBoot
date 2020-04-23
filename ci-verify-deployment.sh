c29bdbccc8473bd5da922618615bfd0667e10837
harishkannarao <harish.gtec@gmail.com>
2020-04-23 16:18:39 +0530

    Enhancement to verification script

--- ci-verify-deployment.sh
+++ ci-verify-deployment.sh
@@ -5,6 +5,8 @@ EXPECTED_SHA=$(echo $GITHUB_SHA | cut -c1-7)
 
 ATTEMPT=1
 MAX_ATTEMPTS=600
+SUCCESS_COUNT=1
+MIN_SUCCESS_COUNT=10
 SLEEP_SECS=1
 SUCCESS=-1
 
@@ -14,13 +16,19 @@ while [ $ATTEMPT -le $MAX_ATTEMPTS ]; do
 
    if [ $SUCCESS == 0 ] && [ $VERSION == "\"${EXPECTED_SHA}\"" ];
    then
-      echo "Deployment appears to be up! Version: ${VERSION} returned from ${VERSION_HOST}"
-      exit 0
+      if [ $SUCCESS_COUNT -le $MIN_SUCCESS_COUNT ]
+      then
+          echo "[${SUCCESS_COUNT}/${MIN_SUCCESS_COUNT}] Deployment appears to be up! Version: ${VERSION} returned from ${VERSION_HOST}"
+          SUCCESS_COUNT=$((SUCCESS_COUNT+1))
+          sleep ${SLEEP_SECS}
+      else
+          exit 0
+      fi
+   else
+      echo "[${ATTEMPT}/${MAX_ATTEMPTS}] Unable to locate version '${EXPECTED_SHA}' at ${VERSION_HOST} - will retry..."
+      sleep ${SLEEP_SECS}
+      ATTEMPT=$((ATTEMPT+1))
    fi
-
-   echo "[${ATTEMPT}/${MAX_ATTEMPTS}] Unable to locate version '${EXPECTED_SHA}' at ${VERSION_HOST} - will retry..."
-   sleep ${SLEEP_SECS}
-   ATTEMPT=$((ATTEMPT+1))
 done
 
 # Unable to verify deployment
