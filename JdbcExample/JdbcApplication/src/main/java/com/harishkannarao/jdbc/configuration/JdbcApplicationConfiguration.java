365af89d894fbb009c59f22dc99ccd065889d803
harishkannarao <harish.gtec@gmail.com>
2020-04-25 14:29:38 +0530

    Adjusting cors settings

--- JdbcExample/JdbcApplication/src/main/java/com/harishkannarao/jdbc/configuration/JdbcApplicationConfiguration.java
+++ JdbcExample/JdbcApplication/src/main/java/com/harishkannarao/jdbc/configuration/JdbcApplicationConfiguration.java
@@ -30,7 +30,7 @@ public class JdbcApplicationConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*").allowedOrigins(corsOrigins.split(","));
+                registry.addMapping("/**").allowedOrigins(corsOrigins.split(",")).allowedMethods("*");
             }
         };
     }
