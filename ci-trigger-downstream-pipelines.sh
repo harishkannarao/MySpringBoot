ad014977df05754e7df60a0957829f329101d2cd
harishkannarao <harish.gtec@gmail.com>
2020-05-03 17:46:14 +0530

    Updating references to run downstream qa acceptance tests

--- ci-trigger-downstream-pipelines.sh
+++ ci-trigger-downstream-pipelines.sh
@@ -6,4 +6,4 @@ set -e
 # Print the commands as it is executed. Useful for debugging
 set -x
 
-curl -v -H "Accept: application/vnd.github.everest-preview+json" -H "Authorization: token $GITHUB_ACCESS_TOKEN" --request POST --data '{"event_type": "do-tag-ci", "client_payload": { "tag": "v2.0"}}' 'https://api.github.com/repos/harishkannarao/kotlin/dispatches'
+curl -v -H "Accept: application/vnd.github.everest-preview+json" -H "Authorization: token $GITHUB_ACCESS_TOKEN" --request POST --data '{"event_type": "do-master-development-ci"}' 'https://api.github.com/repos/harishkannarao/gradle-qa-acceptance-tests/dispatches'
