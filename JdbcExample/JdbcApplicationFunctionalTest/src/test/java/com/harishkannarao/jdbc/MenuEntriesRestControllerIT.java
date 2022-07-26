a91feab854d934bd8f45b261d7b8b97f12f29c84
harishkannarao <harishkannarao@users.noreply.github.com>
2022-07-26 15:01:03 +0100

    Upgrading to Java 17

--- JdbcExample/JdbcApplicationFunctionalTest/src/test/java/com/harishkannarao/jdbc/MenuEntriesRestControllerIT.java
+++ JdbcExample/JdbcApplicationFunctionalTest/src/test/java/com/harishkannarao/jdbc/MenuEntriesRestControllerIT.java
@@ -2,21 +2,23 @@ package com.harishkannarao.jdbc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MenuEntriesRestControllerIT extends BaseIntegrationJdbc {
     @Autowired
-    @org.springframework.beans.factory.annotation.Value("${allMenuEntriesEndpointUrl}")
+    @Value("${allMenuEntriesEndpointUrl}")
     public String allMenuEntriesEndpointUrl;
 
     @Test
     public void getAllMenuEntries_shouldReturnAllMenuEntries_fromDatabase() {
         String[] menuEntriesArray = restTemplate.getForObject(allMenuEntriesEndpointUrl, String[].class);
-        List<String> menuEntries = Arrays.asList(menuEntriesArray);
+        List<String> menuEntries = Arrays.asList(requireNonNull(menuEntriesArray));
         assertEquals(3, menuEntries.size());
     }
 }
