f166f71028691eba3173698a15dd16cac9cc8aef
harishkannarao <harishkannarao@users.noreply.github.com>
2023-11-30 16:19:29 +0000

    Fixing logging issue with Rest Service

--- RestExample/RestServiceAndConsumer/src/main/java/com/harishkannarao/rest/configuration/RestServiceAndConsumerConfiguration.java
+++ RestExample/RestServiceAndConsumer/src/main/java/com/harishkannarao/rest/configuration/RestServiceAndConsumerConfiguration.java
@@ -3,6 +3,8 @@ package com.harishkannarao.rest.configuration;
 import com.harishkannarao.rest.filter.CustomExceptionSimulationFilter;
 import com.harishkannarao.rest.filter.ResponseHeaderFilter;
 import com.harishkannarao.rest.interceptor.request.EvilHeaderRequestInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -18,40 +20,43 @@ import java.util.Collections;
 @Configuration
 public class RestServiceAndConsumerConfiguration {
 
-    @Autowired
-    private EvilHeaderRequestInterceptor evilHeaderRequestInterceptor;
+	private static final Logger log = LoggerFactory.getLogger(RestServiceAndConsumerConfiguration.class);
 
-    @Bean
-    @Qualifier("myRestTemplate")
-    public RestTemplate getRestTemplate() {
-        return new RestTemplate();
-    }
+	@Autowired
+	private EvilHeaderRequestInterceptor evilHeaderRequestInterceptor;
 
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addInterceptors(InterceptorRegistry registry) {
-                registry.addInterceptor(evilHeaderRequestInterceptor);
-            }
-        };
-    }
+	@Bean
+	@Qualifier("myRestTemplate")
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
-    @Bean("responseHeaderFilter")
-    public FilterRegistrationBean<ResponseHeaderFilter> registerResponseHeaderFilter() {
-        FilterRegistrationBean<ResponseHeaderFilter> filterRegistrationBean = new FilterRegistrationBean<>(new ResponseHeaderFilter());
-        filterRegistrationBean.setName(ResponseHeaderFilter.NAME);
-        filterRegistrationBean.setOrder(Ordered.LOWEST_PRECEDENCE);
-        filterRegistrationBean.setUrlPatterns(Collections.singletonList(ResponseHeaderFilter.PATH));
-        return filterRegistrationBean;
-    }
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addInterceptors(InterceptorRegistry registry) {
+				registry.addInterceptor(evilHeaderRequestInterceptor);
+			}
+		};
+	}
 
-    @Bean("customExceptionSimulationFilter")
-    public FilterRegistrationBean<CustomExceptionSimulationFilter> registerCustomExceptionFilterFilter() {
-        FilterRegistrationBean<CustomExceptionSimulationFilter> filterRegistrationBean = new FilterRegistrationBean<>(new CustomExceptionSimulationFilter());
-        filterRegistrationBean.setName(CustomExceptionSimulationFilter.NAME);
-        filterRegistrationBean.setOrder(Ordered.LOWEST_PRECEDENCE);
-        filterRegistrationBean.setUrlPatterns(Collections.singletonList(CustomExceptionSimulationFilter.PATH));
-        return filterRegistrationBean;
-    }
+	@Bean("responseHeaderFilter")
+	public FilterRegistrationBean<ResponseHeaderFilter> registerResponseHeaderFilter() {
+		log.debug("Sample log");
+		FilterRegistrationBean<ResponseHeaderFilter> filterRegistrationBean = new FilterRegistrationBean<>(new ResponseHeaderFilter());
+		filterRegistrationBean.setName(ResponseHeaderFilter.NAME);
+		filterRegistrationBean.setOrder(Ordered.LOWEST_PRECEDENCE);
+		filterRegistrationBean.setUrlPatterns(Collections.singletonList(ResponseHeaderFilter.PATH));
+		return filterRegistrationBean;
+	}
+
+	@Bean("customExceptionSimulationFilter")
+	public FilterRegistrationBean<CustomExceptionSimulationFilter> registerCustomExceptionFilterFilter() {
+		FilterRegistrationBean<CustomExceptionSimulationFilter> filterRegistrationBean = new FilterRegistrationBean<>(new CustomExceptionSimulationFilter());
+		filterRegistrationBean.setName(CustomExceptionSimulationFilter.NAME);
+		filterRegistrationBean.setOrder(Ordered.LOWEST_PRECEDENCE);
+		filterRegistrationBean.setUrlPatterns(Collections.singletonList(CustomExceptionSimulationFilter.PATH));
+		return filterRegistrationBean;
+	}
 }
