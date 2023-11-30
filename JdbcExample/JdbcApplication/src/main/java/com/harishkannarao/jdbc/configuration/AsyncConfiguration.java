24f4b4cbff74aa00ff5aaa7d7c9c2a8fcd541218
harishkannarao <harishkannarao@users.noreply.github.com>
2023-11-30 15:51:20 +0000

    Changind logger level using application.yml

--- JdbcExample/JdbcApplication/src/main/java/com/harishkannarao/jdbc/configuration/AsyncConfiguration.java
+++ JdbcExample/JdbcApplication/src/main/java/com/harishkannarao/jdbc/configuration/AsyncConfiguration.java
@@ -1,5 +1,7 @@
 package com.harishkannarao.jdbc.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,20 +11,24 @@ import java.util.concurrent.Executor;
 
 @Configuration
 public class AsyncConfiguration {
+	private static final Logger log = LoggerFactory.getLogger(AsyncConfiguration.class);
 
-    @Bean("asyncTaskExecutor")
-    public Executor asyncTaskExecutor(
-         @Value("${async.task.executor.pool.size.core}") int corePoolSize,
-         @Value("${async.task.executor.pool.size.max}") int maxPoolSize,
-         @Value("${async.task.executor.queue.capacity}") int queueCapacity
-    ) {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(corePoolSize);
-        executor.setMaxPoolSize(maxPoolSize);
-        executor.setQueueCapacity(queueCapacity);
-        executor.setThreadNamePrefix("AsyncTaskExecutor-");
-        executor.setWaitForTasksToCompleteOnShutdown(true);
-        executor.initialize();
-        return executor;
-    }
+	@Bean("asyncTaskExecutor")
+	public Executor asyncTaskExecutor(
+		@Value("${async.task.executor.pool.size.core}") int corePoolSize,
+		@Value("${async.task.executor.pool.size.max}") int maxPoolSize,
+		@Value("${async.task.executor.queue.capacity}") int queueCapacity
+	) {
+		log.debug("corePoolSize: " + corePoolSize
+			+ " maxPoolSize: " + maxPoolSize
+			+ " queueCapacity: " + queueCapacity);
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(corePoolSize);
+		executor.setMaxPoolSize(maxPoolSize);
+		executor.setQueueCapacity(queueCapacity);
+		executor.setThreadNamePrefix("AsyncTaskExecutor-");
+		executor.setWaitForTasksToCompleteOnShutdown(true);
+		executor.initialize();
+		return executor;
+	}
 }
