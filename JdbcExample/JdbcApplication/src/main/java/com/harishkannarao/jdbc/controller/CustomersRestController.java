63ff8a7cd5ebaec6070bf2c05fef277f2daa275d
harishkannarao <harishkannarao@users.noreply.github.com>
2024-01-04 16:06:35 +0000

    Formatting code

--- JdbcExample/JdbcApplication/src/main/java/com/harishkannarao/jdbc/controller/CustomersRestController.java
+++ JdbcExample/JdbcApplication/src/main/java/com/harishkannarao/jdbc/controller/CustomersRestController.java
@@ -4,12 +4,18 @@ import com.harishkannarao.jdbc.dao.CustomerDao;
 import com.harishkannarao.jdbc.domain.CreateCustomerRequestDto;
 import com.harishkannarao.jdbc.domain.Customer;
 import com.harishkannarao.jdbc.domain.DeleteCustomerRequestDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,45 +25,47 @@ import java.util.Optional;
 @RequestMapping(value = "/customers", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class CustomersRestController extends AbstractBaseController {
 
-    private final CustomerDao customerDao;
+	private static final Logger logger = LoggerFactory.getLogger(CustomersRestController.class);
 
-    @Autowired
-    public CustomersRestController(@Qualifier("myCustomerDao") CustomerDao customerDao) {
-        this.customerDao = customerDao;
-    }
+	private final CustomerDao customerDao;
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Customer> getAllCustomers(
-            @RequestParam("firstName") Optional<String> firstName
-    ) {
-        if (firstName.isPresent()) {
-            return customerDao.getCustomersByFirstName(firstName.get());
-        } else {
-            return customerDao.getAllCustomers();
-        }
-    }
+	@Autowired
+	public CustomersRestController(@Qualifier("myCustomerDao") CustomerDao customerDao) {
+		this.customerDao = customerDao;
+	}
 
-    @RequestMapping(method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Void> createCustomer(
-            @RequestBody CreateCustomerRequestDto requestDto
-    ) {
-        customerDao.createCustomer(requestDto.getFirstName(), requestDto.getLastName());
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Customer> getAllCustomers(
+		@RequestParam("firstName") Optional<String> firstName
+	) {
+		if (firstName.isPresent()) {
+			return customerDao.getCustomersByFirstName(firstName.get());
+		} else {
+			return customerDao.getAllCustomers();
+		}
+	}
 
-    @RequestMapping(method = RequestMethod.PUT, consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Void> createCustomerWithTransaction(
-            @RequestBody CreateCustomerRequestDto requestDto
-    ) {
-        customerDao.createCustomer(requestDto.getFirstName(), requestDto.getLastName());
-        throw new RuntimeException("Bang Bang");
-    }
+	@RequestMapping(method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<Void> createCustomer(
+		@RequestBody CreateCustomerRequestDto requestDto
+	) {
+		customerDao.createCustomer(requestDto.getFirstName(), requestDto.getLastName());
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 
-    @RequestMapping(method = RequestMethod.DELETE, consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Void> deleteCustomer(
-            @RequestBody DeleteCustomerRequestDto requestDto
-    ) {
-        customerDao.deleteCustomer(requestDto.getId());
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+	@RequestMapping(method = RequestMethod.PUT, consumes = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<Void> createCustomerWithTransaction(
+		@RequestBody CreateCustomerRequestDto requestDto
+	) {
+		customerDao.createCustomer(requestDto.getFirstName(), requestDto.getLastName());
+		throw new RuntimeException("Bang Bang");
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, consumes = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<Void> deleteCustomer(
+		@RequestBody DeleteCustomerRequestDto requestDto
+	) {
+		customerDao.deleteCustomer(requestDto.getId());
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 }
