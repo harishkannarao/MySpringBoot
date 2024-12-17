acdd8e17a6d4f6a6c34be71c77440ec5e4a76e64
harishkannarao <harishkannarao@users.noreply.github.com>
2024-12-17 20:47:21 +0000

    Removing AbstractBaseController.java

--- JdbcExample/JdbcApplication/src/main/java/com/harishkannarao/jdbc/controller/AbstractBaseController.java
+++
@@ -1,8 +0,0 @@
-package com.harishkannarao.jdbc.controller;
-
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-@Transactional
-public abstract class AbstractBaseController {
-}
