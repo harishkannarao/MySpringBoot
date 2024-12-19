258df166861ab06c10c8e93fbcf61d566249f9a5
harishkannarao <harishkannarao@users.noreply.github.com>
2024-12-19 22:32:14 +0000

    Integration test to verify booking after reservation

--- JdbcExample/JdbcApplicationFunctionalTest/src/test/java/com/harishkannarao/jdbc/TicketReservationIT.java
+++ JdbcExample/JdbcApplicationFunctionalTest/src/test/java/com/harishkannarao/jdbc/TicketReservationIT.java
@@ -1,7 +1,9 @@
 package com.harishkannarao.jdbc;
 
+import com.harishkannarao.jdbc.domain.TicketBookingResponseDto;
 import com.harishkannarao.jdbc.domain.TicketReservationResponseDto;
 import com.harishkannarao.jdbc.domain.TicketsAvailabilityResponseDto;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +13,10 @@ import org.springframework.http.ResponseEntity;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static java.util.Objects.requireNonNull;
@@ -24,17 +28,20 @@ public class TicketReservationIT extends BaseIntegrationJdbc {
 	private final String createTicketsEndpoint;
 	private final String getTicketsAvailabilityEndpoint;
 	private final String reserveTicketEndpoint;
+	private final String bookTicketEndpoint;
 
 	@Autowired
 	public TicketReservationIT(
 		TicketTestSupportDao ticketTestSupportDao,
 		@Value("${createTicketEndpointUrl}") String createTicketsEndpoint,
 		@Value("${getTicketsAvailabilityEndpointUrl}") String getTicketsAvailabilityEndpoint,
-		@Value("${reserveTicketEndpointUrl}") String reserveTicketEndpoint) {
+		@Value("${reserveTicketEndpointUrl}") String reserveTicketEndpoint,
+		@Value("${bookTicketEndpointUrl}") String bookTicketEndpoint) {
 		this.ticketTestSupportDao = ticketTestSupportDao;
 		this.createTicketsEndpoint = createTicketsEndpoint;
 		this.getTicketsAvailabilityEndpoint = getTicketsAvailabilityEndpoint;
 		this.reserveTicketEndpoint = reserveTicketEndpoint;
+		this.bookTicketEndpoint = bookTicketEndpoint;
 	}
 
 	@BeforeEach
@@ -43,7 +50,7 @@ public class TicketReservationIT extends BaseIntegrationJdbc {
 	}
 
 	@Test
-	public void create_tickets_and_reserve_tickets_without_loss() {
+	public void create_tickets_and_reserve_tickets_and_book_tickets_without_loss() {
 		ResponseEntity<String[]> createResp = restClient
 			.put()
 			.uri(createTicketsEndpoint, Map.of("count", 100L))
@@ -104,5 +111,52 @@ public class TicketReservationIT extends BaseIntegrationJdbc {
 
 		assertThat(availableTicketsAfterReservation.getStatusCode().value()).isEqualTo(200);
 		assertThat(requireNonNull(availableTicketsAfterReservation.getBody()).available()).isEqualTo(0);
+
+		var bookingRequests = reservationResponses.stream()
+			.parallel()
+			.map(entity -> requireNonNull(entity.getBody()))
+			.map(reservation -> CompletableFuture.supplyAsync(() ->
+				restClient
+					.post()
+					.uri(bookTicketEndpoint, reservation.ticketId(), reservation.customerId())
+					.retrieve()
+					.toEntity(TicketBookingResponseDto.class))
+			)
+			.toList();
+
+		List<ResponseEntity<TicketBookingResponseDto>> bookingResponses = bookingRequests.stream()
+			.parallel()
+			.map(CompletableFuture::join)
+			.toList();
+
+		assertThat(bookingResponses)
+			.allSatisfy(it ->
+				assertThat(it.getStatusCode().value()).isEqualTo(200));
+
+		Map<Boolean, List<TicketBookingResponseDto>> bookingsMapping = bookingResponses.stream()
+			.map(it -> requireNonNull(it.getBody()))
+			.collect(Collectors.partitioningBy(o -> o.customerId() != null));
+
+		List<TicketBookingResponseDto> successfulBookings = bookingsMapping.get(true);
+		List<TicketBookingResponseDto> unsuccessfulBookings = bookingsMapping.get(false);
+
+		assertThat(successfulBookings)
+			.hasSameSizeAs(reservedTickedIds);
+		assertThat(unsuccessfulBookings).isEmpty();
+
+		Set<UUID> uniqueCustomerIds = successfulBookings.stream()
+			.map(TicketBookingResponseDto::customerId)
+			.collect(Collectors.toUnmodifiableSet());
+
+		assertThat(uniqueCustomerIds).hasSize(100);
+
+		for(var reservationResponse : reservationResponses) {
+			var reservation = requireNonNull(reservationResponse.getBody());
+			assertThat(successfulBookings)
+				.anySatisfy(booking -> {
+					assertThat(booking.ticketId()).isEqualTo(reservation.ticketId());
+					assertThat(booking.customerId()).isEqualTo(reservation.customerId());
+				});
+		}
 	}
 }
