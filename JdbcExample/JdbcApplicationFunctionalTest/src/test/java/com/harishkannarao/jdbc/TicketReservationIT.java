23cfaa71da7ccb489f7453e8412fb777d18adb69
harishkannarao <harishkannarao@users.noreply.github.com>
2024-12-21 11:31:22 +0000

    Integration test to verify integrity after reservation

--- JdbcExample/JdbcApplicationFunctionalTest/src/test/java/com/harishkannarao/jdbc/TicketReservationIT.java
+++ JdbcExample/JdbcApplicationFunctionalTest/src/test/java/com/harishkannarao/jdbc/TicketReservationIT.java
@@ -159,4 +159,86 @@ public class TicketReservationIT extends BaseIntegrationJdbc {
 				});
 		}
 	}
+
+	@Test
+	public void reserve_tickets_should_book_with_integrity() {
+		ResponseEntity<String[]> createResp = restClient
+			.put()
+			.uri(createTicketsEndpoint, Map.of("count", 100L))
+			.retrieve()
+			.toEntity(String[].class);
+
+		assertThat(createResp.getStatusCode().value()).isEqualTo(200);
+		List<String> createdTicketIds = Arrays.stream(requireNonNull(createResp.getBody())).toList();
+		assertThat(createdTicketIds).hasSize(100);
+
+		ResponseEntity<TicketsAvailabilityResponseDto> availableTicketsBeforeReservation = restClient
+			.get()
+			.uri(getTicketsAvailabilityEndpoint)
+			.retrieve()
+			.toEntity(TicketsAvailabilityResponseDto.class);
+
+		assertThat(availableTicketsBeforeReservation.getStatusCode().value()).isEqualTo(200);
+		assertThat(requireNonNull(availableTicketsBeforeReservation.getBody()).available())
+			.isEqualTo(100);
+
+		var reservationRequests =
+			IntStream.rangeClosed(1, 150)
+				.boxed()
+				.parallel()
+				.map(integer ->
+					CompletableFuture.supplyAsync(() ->
+						restClient
+							.post()
+							.uri(reserveTicketEndpoint, UUID.randomUUID().toString())
+							.retrieve()
+							.toEntity(TicketReservationResponseDto.class)))
+				.toList();
+
+		var reservationResponses = reservationRequests.stream()
+			.map(CompletableFuture::join)
+			.toList();
+
+		assertThat(reservationResponses)
+			.allSatisfy(resp -> {
+				assertThat(resp.getStatusCode().value()).isEqualTo(200);
+			});
+
+
+		Map<Boolean, List<TicketReservationResponseDto>> reservationResponse = reservationResponses
+			.stream()
+			.map(resp -> requireNonNull(resp.getBody()))
+			.collect(Collectors.partitioningBy(o -> o.ticketId() != null));
+
+		List<TicketReservationResponseDto> successfulReservations = reservationResponse.get(true);
+		List<TicketReservationResponseDto> unSuccessfulReservations = reservationResponse.get(false);
+
+		assertThat(successfulReservations)
+			.hasSize(100)
+			.allSatisfy(it -> {
+				assertThat(it.ticketId()).isNotNull();
+				assertThat(it.customerId()).isNotNull();
+			});
+
+		assertThat(unSuccessfulReservations)
+			.hasSize(50)
+			.allSatisfy(it -> {
+				assertThat(it.ticketId()).isNull();
+				assertThat(it.customerId()).isNotNull();
+			});
+
+		Set<TicketReservationResponseDto> uniqueReservations = successfulReservations.stream()
+			.collect(Collectors.toUnmodifiableSet());
+		assertThat(uniqueReservations).hasSize(100);
+
+		Set<UUID> uniqueReservedTicketIds = successfulReservations.stream()
+			.map(TicketReservationResponseDto::ticketId)
+			.collect(Collectors.toUnmodifiableSet());
+		assertThat(uniqueReservedTicketIds).hasSize(100);
+
+		Set<UUID> uniqueReservedCustomerIds = successfulReservations.stream()
+			.map(TicketReservationResponseDto::customerId)
+			.collect(Collectors.toUnmodifiableSet());
+		assertThat(uniqueReservedCustomerIds).hasSize(100);
+	}
 }
