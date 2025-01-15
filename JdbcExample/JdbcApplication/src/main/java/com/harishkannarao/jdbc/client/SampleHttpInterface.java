3cde53372e79ed0242493b79c9b4eb28d83394d8
harishkannarao <harishkannarao@users.noreply.github.com>
2025-01-15 23:27:46 +0000

    Imports clean up

--- JdbcExample/JdbcApplication/src/main/java/com/harishkannarao/jdbc/client/SampleHttpInterface.java
+++ JdbcExample/JdbcApplication/src/main/java/com/harishkannarao/jdbc/client/SampleHttpInterface.java
@@ -1,7 +1,6 @@
 package com.harishkannarao.jdbc.client;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
