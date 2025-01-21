3b58e97228f4b9d1539f798ecb85ebeaefb093ea
harishkannarao <harishkannarao@users.noreply.github.com>
2025-01-21 16:08:50 +0000

    Auto-injecting converters

--- JdbcExample/JdbcApplication/src/main/java/com/harishkannarao/jdbc/configuration/DataJdbcConfiguration.java
+++ JdbcExample/JdbcApplication/src/main/java/com/harishkannarao/jdbc/configuration/DataJdbcConfiguration.java
@@ -1,11 +1,11 @@
 package com.harishkannarao.jdbc.configuration;
 
-import com.harishkannarao.jdbc.entity.type.JsonContentReader;
-import com.harishkannarao.jdbc.entity.type.JsonContentWriter;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
@@ -15,7 +15,6 @@ import org.springframework.jdbc.support.JdbcTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -26,6 +25,13 @@ import java.util.List;
 	transactionManagerRef = "myTransactionManager"
 )
 public class DataJdbcConfiguration extends AbstractJdbcConfiguration {
+
+	private final List<Converter<?, ?>> converters;
+
+	@Autowired
+	public DataJdbcConfiguration(List<Converter<?, ?>> converters) {
+		this.converters = converters;
+	}
 
 	@Bean(name = "myJdbcOperations")
 	public NamedParameterJdbcOperations createNamedParameterJdbcOperations(
@@ -42,6 +48,6 @@ public class DataJdbcConfiguration extends AbstractJdbcConfiguration {
 	@NotNull
 	@Override
 	protected List<?> userConverters() {
-		return Arrays.asList(new JsonContentWriter(), new JsonContentReader());
+		return List.copyOf(converters);
 	}
 }
