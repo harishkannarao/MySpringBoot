66a42ef7c368e4145e7c902bbc48a55cb14099c2
harishkannarao <harishkannarao@users.noreply.github.com>
2025-03-22 09:59:05 +0000

    Pulling docker image before maven build

--- ci-build.sh
+++ ci-build.sh
@@ -6,4 +6,6 @@ set -e
 # Print the commands as it is executed. Useful for debugging
 set -x
 
+docker pull public.ecr.aws/docker/library/postgres:16
+
 mvn clean install --batch-mode -P run-in-ci -DskipDockerBuild=true
