b71df94ea2dc06db7d91af30a63db5592d6d77ba
harishkannarao <harishkannarao@users.noreply.github.com>
2025-11-10 14:57:29 +0000

    List assertion

--- JdbcExample/JdbcApplication/src/test/java/com/harishkannarao/jdbc/domain/DeeplyImmutableRecordTest.java
+++ JdbcExample/JdbcApplication/src/test/java/com/harishkannarao/jdbc/domain/DeeplyImmutableRecordTest.java
@@ -1,5 +1,6 @@
 package com.harishkannarao.jdbc.domain;
 
+import org.assertj.core.api.recursive.comparison.RecursiveComparisonConfiguration;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -33,5 +34,42 @@ public class DeeplyImmutableRecordTest {
 			.ignoringFields("text")
 			.ignoringCollectionOrder()
 			.isEqualTo(input);
+	}
+
+	@Test
+	public void test_list_assertion() {
+		final DeeplyImmutableRecord record1 = new DeeplyImmutableRecord(
+			2,
+			"test-name-2",
+			Optional.of("optional-text"),
+			List.of("abc", "def"),
+			Set.of("def", "ghi"),
+			Map.of("ghi", "jkl")
+		);
+		final DeeplyImmutableRecord record1Copy = new DeeplyImmutableRecord(
+			2,
+			"test-name-2",
+			Optional.of("optional-text"),
+			List.of("def", "abc"),
+			Set.of("ghi", "def"),
+			Map.of("ghi", "jkl")
+		);
+		final DeeplyImmutableRecord record2 = new DeeplyImmutableRecord(
+			3,
+			"test-name-3",
+			Optional.empty(),
+			null,
+			null,
+			null
+		);
+		final DeeplyImmutableRecord record2Copy = new DeeplyImmutableRecordBuilder(record2).build();
+
+		List<DeeplyImmutableRecord> actual = List.of(record1, record2);
+		List<DeeplyImmutableRecord> expected = List.of(record1Copy, record2Copy);
+
+		assertThat(actual)
+			.usingRecursiveFieldByFieldElementComparator(
+				RecursiveComparisonConfiguration.builder().withIgnoreCollectionOrder(true).build())
+			.containsExactlyElementsOf(expected);
 	}
 }
