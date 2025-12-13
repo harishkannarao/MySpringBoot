a6e6b1b4cc238ee8e6a3d8424ee78d004bd20aca
harishkannarao <harishkannarao@users.noreply.github.com>
2025-12-13 09:18:30 +0000

    More repository test

--- JdbcExample/JdbcApplicationFunctionalTest/src/test/java/com/harishkannarao/jdbc/OrderRepositoryTest.java
+++ JdbcExample/JdbcApplicationFunctionalTest/src/test/java/com/harishkannarao/jdbc/OrderRepositoryTest.java
@@ -2,19 +2,23 @@ package com.harishkannarao.jdbc;
 
 import com.harishkannarao.jdbc.dao.repository.OrderRepository;
 import com.harishkannarao.jdbc.entity.Order;
-import org.assertj.core.api.Assertions;
+import com.harishkannarao.jdbc.entity.OrderBuilder;
+import org.assertj.core.api.recursive.comparison.RecursiveComparisonConfiguration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
 
 @SpringBootTest
 @ActiveProfiles({"default", "h2-db"})
@@ -37,25 +41,158 @@ public class OrderRepositoryTest {
 	}
 
 	@Test
-	public void simple() {
-		Order order = new Order(2L, UUID.randomUUID(), null, null, null);
-		orderRepository.save(order);
-		List<Order> dbEntities = orderRepository.findAll();
-		assertThat(dbEntities)
-			.hasSize(1)
-			.anySatisfy(actual -> {
-				assertThat(actual)
+	public void test_create_read_update_delete_order() {
+		Order input = new Order(null, UUID.randomUUID(), null, null, null);
+		Long id = orderRepository.save(input).id();
+		Order created = orderRepository.findById(id).orElseThrow();
+
+		assertThat(created.id()).isNotNull();
+		assertThat(created.customerId()).isEqualTo(input.customerId());
+		assertThat(created.createdTime())
+			.isAfterOrEqualTo(Instant.now().minusSeconds(2))
+			.isBeforeOrEqualTo(Instant.now().plusSeconds(2));
+		assertThat(created.updatedTime())
+			.isAfterOrEqualTo(Instant.now().minusSeconds(2))
+			.isBeforeOrEqualTo(Instant.now().plusSeconds(2));
+		assertThat(created.version()).isEqualTo(0);
+
+		Order toUpdate = OrderBuilder.from(created)
+			.customerId(UUID.randomUUID())
+			.build();
+
+		orderRepository.save(toUpdate);
+		Order updated = orderRepository.findById(toUpdate.id()).orElseThrow();
+
+		assertThat(updated.id()).isEqualTo(created.id());
+		assertThat(updated.customerId()).isEqualTo(toUpdate.customerId());
+		assertThat(updated.createdTime())
+			.isEqualTo(created.createdTime());
+		assertThat(updated.updatedTime())
+			.isAfterOrEqualTo(created.updatedTime())
+			.isBeforeOrEqualTo(Instant.now().plusSeconds(2));
+		assertThat(updated.version()).isEqualTo(1);
+
+		Optional<Order> read = orderRepository.findById(created.id());
+
+		assertThat(read)
+			.isPresent()
+			.hasValueSatisfying(value ->
+				assertThat(value)
 					.usingRecursiveComparison()
 					.ignoringCollectionOrder()
-					.ignoringFields("createdTime", "updatedTime", "version")
-					.isEqualTo(order);
-				assertThat(actual.createdTime())
-					.isAfterOrEqualTo(Instant.now().minusSeconds(2))
-					.isBeforeOrEqualTo(Instant.now().plusSeconds(2));
-				assertThat(actual.updatedTime())
-					.isAfterOrEqualTo(Instant.now().minusSeconds(2))
-					.isBeforeOrEqualTo(Instant.now().plusSeconds(2));
-				assertThat(actual.version()).isEqualTo(0);
-			});
+					.isEqualTo(updated));
+
+		orderRepository.deleteById(created.id());
+
+		Optional<Order> afterDelete = orderRepository.findById(created.id());
+		assertThat(afterDelete).isEmpty();
+	}
+
+	@Test
+	public void test_optimistic_error_on_version_mismatch() {
+		Order input = new Order(null, UUID.randomUUID(), null, null, null);
+
+		Order created = orderRepository.save(input);
+		assertThat(created.version()).isEqualTo(0);
+
+		Order updated = orderRepository.save(created);
+		assertThat(updated.version()).isEqualTo(1);
+
+		Order staleUpdate = OrderBuilder.from(created)
+			.customerId(UUID.randomUUID())
+			.build();
+
+		OptimisticLockingFailureException staleResult = catchThrowableOfType(
+			() -> orderRepository.save(staleUpdate),
+			OptimisticLockingFailureException.class);
+		assertThat(staleResult).isNotNull();
+
+		Order futureUpdate = OrderBuilder.from(updated)
+			.customerId(UUID.randomUUID())
+			.version(updated.version() + 1)
+			.build();
+
+		OptimisticLockingFailureException futureResult = catchThrowableOfType(
+			() -> orderRepository.save(futureUpdate),
+			OptimisticLockingFailureException.class);
+		assertThat(futureResult).isNotNull();
+	}
+
+	@Test
+	public void test_findAll_by_customer_id() {
+		UUID customerId = UUID.randomUUID();
+
+		Long orderId1 = orderRepository.save(
+				new Order(null, customerId, null, null, null))
+			.id();
+		Order order1 = orderRepository.findById(orderId1).orElseThrow();
+		Long orderId2 = orderRepository.save(
+				new Order(null, customerId, null, null, null))
+			.id();
+		Order order2 = orderRepository.findById(orderId2).orElseThrow();
+		orderRepository.save(
+			new Order(null, UUID.randomUUID(), null, null, null));
+
+		List<Order> result = orderRepository.findByCustomerId(customerId);
+
+		assertThat(result)
+			.usingRecursiveFieldByFieldElementComparator(
+				RecursiveComparisonConfiguration.builder().withIgnoreCollectionOrder(true).build())
+			.containsExactlyInAnyOrderElementsOf(List.of(order1, order2));
+	}
+
+	@Test
+	public void test_findAll_by_customer_ids() {
+		UUID customerId1 = UUID.randomUUID();
+		UUID customerId2 = UUID.randomUUID();
+
+		Long orderId1 = orderRepository.save(
+				new Order(null, customerId1, null, null, null))
+			.id();
+		Order order1 = orderRepository.findById(orderId1).orElseThrow();
+		Long orderId2 = orderRepository.save(
+				new Order(null, customerId1, null, null, null))
+			.id();
+		Order order2 = orderRepository.findById(orderId2).orElseThrow();
+		Long orderId3 = orderRepository.save(
+				new Order(null, customerId2, null, null, null))
+			.id();
+		Order order3 = orderRepository.findById(orderId3).orElseThrow();
+		orderRepository.save(
+			new Order(null, UUID.randomUUID(), null, null, null));
+
+		List<Order> result = orderRepository.findAllByCustomerIds(List.of(customerId1, customerId2));
+
+		assertThat(result)
+			.usingRecursiveFieldByFieldElementComparator(
+				RecursiveComparisonConfiguration.builder().withIgnoreCollectionOrder(true).build())
+			.containsExactlyInAnyOrderElementsOf(List.of(order1, order2, order3));
+	}
+
+	@Test
+	public void test_delete_by_customer_ids() {
+		UUID customerId1 = UUID.randomUUID();
+		UUID customerId2 = UUID.randomUUID();
+		UUID customerId3 = UUID.randomUUID();
+
+		Order order1 = orderRepository.save(
+			new Order(null, customerId1, null, null, null));
+		Order order2 = orderRepository.save(
+			new Order(null, customerId1, null, null, null));
+		Order order3 = orderRepository.save(
+			new Order(null, customerId2, null, null, null));
+		Order order4 = orderRepository.save(
+			new Order(null, customerId3, null, null, null));
+
+		orderRepository.deleteAllByCustomerIdIn(
+			List.of(customerId1, customerId2));
+
+		List<Order> result = orderRepository.findAllById(
+			List.of(order1.id(), order2.id(), order3.id()));
+
+		assertThat(result).isEmpty();
+
+		assertThat(orderRepository.findById(order4.id()))
+			.isPresent();
 	}
 }
